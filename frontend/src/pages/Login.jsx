c54168ebe03d3fc33851c9e401c421defcc3a37c
Ace-shhh <ace268686@gmail.com>
2024-12-26 18:16:32 +1300

    Update login with test span

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -100,6 +100,7 @@ const Login = () =>{
                 <button className='google-sign-in-button' onClick={handleGoogleLogin}>
                 <img src={GoogleIcon} alt='Sign in using Google'/>
                 </button>
+                <span>Pull request test</span>
             </form>
 
         </div>
